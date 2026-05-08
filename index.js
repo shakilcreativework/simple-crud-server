900a6759541884a4447c178d03a50d4e710c40c6
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-08 11:35:10 +0600

    now delete user confirm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // mongodb+srv://simpleCRUD:<db_password>@cluster0.68fcvxk.mongodb.net/?appName=Cluster0
@@ -32,13 +32,33 @@ const run = async () => {
   try {
     await client.connect();
     // --------------start code--------------
-    const db = client.db('simpleCrud');
-    const userCollection = db.collection('users');
+    const db = client.db("simpleCrud");
+    const userCollection = db.collection("users");
 
     // get users
-    app.get('/users', async (req, res) => {
+    app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const user = await userCollection.findOne(query);
+      console.log("user id", id);
+      res.send(user);
+    });
+
+    // delete
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
     // --------------ends code---------------
